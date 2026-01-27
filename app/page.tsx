8f893efe57cdc2796a7bea2cc708bcedf655a66a
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-01-27 22:50:52 +0100

    Structured Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
-import Navbar from "@/app/components/layout/Navbar";
+import Navbar from "@/components/layout/Navbar";
+import HomeHero from "@/components/sections/home/HomeHero";
+import WobbleCardSection from "@/components/sections/home/WobbleCardSection";
+import {Separator} from "@/components/ui/separator";
+import {InfiniteMovingCards} from "@/components/ui/infinite-moving-cards";
+import InfiniteScrollSection from "@/components/sections/home/InfiniteScrollSection";
+import MinibusSection from "@/components/sections/home/MinibusSection";
+
+
 
 export default function Home() {
-  return (
-    <main>
-        <Navbar />
+
+
+    return (
+    <main className="bg-black w-full flex flex-col items-center">
+        <div className="flex flex-col max-w-[1440px] mx-auto w-full">
+            <Navbar />
+
+            <HomeHero />
+            <WobbleCardSection />
+            <InfiniteScrollSection />
+            <MinibusSection />
+            <div className="min-h-screen"></div>
+        </div>
     </main>
   );
 }
