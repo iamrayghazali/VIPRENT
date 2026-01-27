b21c9504f2091cacc1604b4c627f14b7f17cb351
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-01-27 22:49:02 +0100

    added temporary hero

---
+++ components/sections/home/HomeHero.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import {FlipWords} from "@/components/ui/flip-words";
+import {TextHoverEffect} from "@/components/ui/text-hover-effect";
+import homeHeroCarBg from "../../../public/hero/home-hero-car-bg.svg"
+import BG from "../../../public/gallery/mercedes-gls-63-luxusauto-berles.jpg";
+
+export default function HomeHero() {
+    return (
+        <section className="flex flex-col justify-center  min-h-screen ">
+            <TextHoverEffect text="VIPRENT" className="text-wrap"></TextHoverEffect>
+            {/*<Image src={BG} alt="BG" className="absolute top-0 h-screen"></Image>*/}
+            {/*<FlipWords words={["LUXURY", "SPORT", "COMFORT"]}></FlipWords>*/}
+        </section>
+    );
+}
