680215975a29ed63b9eb2c3deadf226bbc3010c3
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-01-29 01:57:50 +0100

    Created about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,22 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
+
+
 export const metadata = {
     title: "About VIPRENT",
     description: "Luxury and sports car rental company.",
 };
+
+export default function About() {
+    return (
+        <main className="bg-black w-full flex flex-col items-center">
+            <div className="flex flex-col max-w-[1440px] mx-auto w-full">
+                <Navbar />
+
+
+                <Footer />
+            </div>
+        </main>
+    )
+}
