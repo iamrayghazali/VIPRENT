b331d6e59d3138b229a768f45b0af015b6e43ed3
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-02-07 23:37:46 +0100

    Added initial AboutHero.tsx layout

--- components/sections/about/AboutHero.tsx
+++ components/sections/about/AboutHero.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import {motion} from "framer-motion";
+import Image from "next/image";
+import {FlipWords} from "@/components/ui/flip-words";
+import {TextHoverEffect} from "@/components/ui/text-hover-effect";
 
 
 export default function AboutHero() {
     return (
-        <div className="">
-            
+        <div className="relative w-full min-h-[80vh] md:min-h-screen overflow-hidden">
+            {/* Background image with fade-in */}
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 5 }}
+                className="absolute inset-0"
+            >
+                <Image
+                    src="/hero/car-outline-bg.jpg"
+                    alt="Background Image"
+                    fill
+                    className="object-cover object-center"
+                    priority
+                />
+            </motion.div>
         </div>
     )
 }
