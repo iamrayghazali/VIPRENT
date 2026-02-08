fd2952ce2f1ba810b5dbd5581eaf99043e492803
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-02-08 04:12:01 +0100

    Added float button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import WobbleCardSection from "@/components/sections/home/WobbleCardSection";
 import InfiniteScrollSection from "@/components/sections/home/InfiniteScrollSection";
 import MinibusSection from "@/components/sections/home/MinibusSection";
 import Footer from "@/components/layout/Footer";
+import FloatingCallButton from "@/components/ui/FloatingContactButton";
 
 
 
@@ -14,7 +15,7 @@ export default function Home() {
     <main className="bg-black w-full flex flex-col items-center">
         <div className="flex flex-col max-w-[1440px] mx-auto w-full">
             <Navbar />
-
+            <FloatingCallButton></FloatingCallButton>
             <HomeHero />
             <WobbleCardSection />
             <InfiniteScrollSection />
